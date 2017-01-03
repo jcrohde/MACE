746b21c313ffabb18b0d40827c7a6860ac1c6288
Jan Christian Rohde <jan-christian.rohde@gmx.de>
2017-01-03 22:58:54 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,30 +1,29 @@
 /*
-Copyright (C) 2012 Jan Christian Rohde
+Copyright (C) 2012-2014 Jan Christian Rohde
 
 This file is part of MACE.
 
-MACE is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+MACE is free software; you can redistribute it and/or modify it under the terms of the
+GNU General Public License as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
 
-MACE is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
+MACE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public License
-along with MACE. If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with MACE; if not, see http://www.gnu.org/licenses.
 */
 
-#include <QtGui/QApplication>
-#include "mainwindow.h"
+#include <QApplication>
+#include "gui/macewindow.h"
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+int main(int argc, char **argv) {
 
-    return a.exec();
+        QApplication app(argc, argv);
+
+        MaceWindow window;
+
+        window.show();
+        return app.exec();
 }
